46f7a3da48dca4309c2fcecd87e165cc24583843
Florian Sellmayr <florian.sellmayr@gmail.com>
2015-07-25 18:28:36 +0200

    wip: copy artifacts from build working directory to some other place in the home-dir

--- src/lambdacd_artifacts/core.clj
+++ src/lambdacd_artifacts/core.clj
@@ -1,4 +1,7 @@
-(ns lambdacd-artifacts.core)
+(ns lambdacd-artifacts.core
+  (:require [clojure.java.io :as io]))
 
-(defn dummy [arg ctx]
-  {:status :success})
+(defn publish-artifacts [working-directory patterns args ctx]
+  (io/copy (io/file working-directory (first patterns)) (io/file (:home-dir (:config ctx)) (first patterns)))
+  {:status :success
+   :details []})
--- test/lambdacd_artifacts/core_test.clj
+++ test/lambdacd_artifacts/core_test.clj
@@ -1,7 +1,28 @@
 (ns lambdacd-artifacts.core-test
   (:require [clojure.test :refer :all]
-            [lambdacd-artifacts.core :refer :all]))
+            [lambdacd-artifacts.core :refer :all]
+            [clojure.java.io :as io]
+            [lambdacd.util :as util]))
 
-(deftest dummy-test
-  (testing "that the test runs"
-    (is (= {:status :success} (dummy nil nil)))))
+(defn map-containing [expected m]
+  (and (every? (set (keys m)) (keys expected))
+       (every? #(= (m %)(expected %)) (keys expected))))
+
+(defn- file-in [dir]
+  (->> (file-seq (io/file dir))
+       (filter #(.isFile %))))
+
+(deftest publish-artifacts-test
+  (testing "that a specified file is copied to the archive folder"
+    (let [cwd      (util/create-temp-dir)
+          home-dir (util/create-temp-dir)]
+      (spit (io/file cwd "foo.txt") "hello content")
+      (let [publish-artifacts-result (publish-artifacts cwd ["foo.txt"] {} {:config {:home-dir home-dir}})]
+        (is (map-containing {:status :success} publish-artifacts-result))
+        (is (= "hello content" (slurp (first (file-in home-dir)))))))))
+
+;; TODO: artifact should be in some defined folder in the home-dir
+;; TODO: more than one pattern should be supported
+;; TODO: actual patterns should be supported
+;; TODO: ring handler to read file again needs to be implemented, add an integration test here that first publishes an artifact,
+;;       then reads it from the ring handler
